183b3d13f892ddbd1e52e492654e78c651e9b09f
Junya Hosokawa <hosokawa@morijyobi.com>
2016-11-19 10:27:44 +0900

    PhysicsSpriteBodyが不良なのでSpriteに変更して　Update関数を修正

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 //衝突タイプ（衝突時の判別用）
-if(typeof SpriteTag == "undefined") {
-	var SpriteTag = {};
-	SpriteTag.totem = 0; // トーテム
-	SpriteTag.block = 1; // ブロック
-	SpriteTag.object = 2; //オブジェ
+if (typeof SpriteTag == "undefined") {
+   var SpriteTag = {};
+   SpriteTag.totem = 0; // トーテム
+   SpriteTag.block = 1; // ブロック
+   SpriteTag.object = 2; //オブジェ
 };
 
 
@@ -18,7 +18,11 @@ var gameScene = cc.Scene.extend({
 
 var totem;
 
+var shapeArray = [];
+
 var blocks = [];
+
+var g_groundHeight = 10;
 
 var game = cc.Layer.extend({
    space: null,
@@ -29,22 +33,22 @@ var game = cc.Layer.extend({
       this.addChild(backgroundLayer);
 
       this.initSpace();
+
       var winWidth = cc.winSize.width
       var winHeight = cc.winSize.height
-      totem =  this.createDynamicObject(res.totem_png, winWidth*0.4 , winHeight, 1, 0.2, 0.8, "")
-     this.createStaticObject(res.ground_png, cc.winSize.width / 2, 100);
+      totem = this.createDynamicObject(res.totem_png,
+				 winWidth * 0.4, winHeight, 1, 0.2, 0.8, SpriteTag.totem)
 
-blocks[0] = this.createDynamicObject(res.brick1x1_png, winWidth* 0.7,120,1, 10, 0.8, "")
-blocks[1] = this.createDynamicObject(res.brick1x1_png, winWidth* 0.7,120+25*1,1, 10, 0.8, "")
-blocks[2] = this.createDynamicObject(res.brick1x1_png, winWidth* 0.7,120+25*2,1, 10, 0.8, "")
-blocks[3] = this.createDynamicObject(res.brick1x1_png, winWidth* 0.7,120+25*3,1, 10, 0.8, "")
+      this.createStaticObject(res.ground_png, cc.winSize.width / 2, 100);
 
-      cc.eventManager.addListener(touchListener,this)
+      blocks[0] = this.createDynamicObject(res.brick1x1_png, winWidth * 0.7, 120, 1, 10, 0.8,  SpriteTag.block)
+      blocks[1] = this.createDynamicObject(res.brick2x1_png, winWidth * 0.7, 120 + 25 * 1, 1, 10, 0.8,  SpriteTag.block)
+      blocks[2] = this.createDynamicObject(res.brick1x1_png, winWidth * 0.7, 120 + 25 * 2, 1, 10, 0.8, SpriteTag.block)
+      blocks[3] = this.createDynamicObject(res.brick1x1_png, winWidth * 0.7, 120 + 25 * 3, 1, 10, 0.8,  SpriteTag.block)
+
+      cc.eventManager.addListener(touchListener, this)
 
    },
-
-
-
 
    createStaticObject: function(spriteImage, posX, posY) {
       var staticSprite = cc.Sprite.create(spriteImage);
@@ -66,34 +70,29 @@ blocks[3] = this.createDynamicObject(res.brick1x1_png, winWidth* 0.7,120+25*3,1,
 
    createDynamicObject: function(spriteImage, posX, posY, mass, friction, elasticity, type) {
 
-      // 物理スプライト
-      var physicsSprite = new cc.PhysicsSprite(spriteImage);
 
-      // 質量
-      // var mass = 100;
-
-      // スプライトの大きさを取得
-      var width = physicsSprite.getContentSize().width;
-      var height = physicsSprite.getContentSize().height;
+  		var sprite = new cc.Sprite.create(spriteImage);
+	    var width = sprite.getContentSize().width;
+      var height = sprite.getContentSize().height;
 
       var body = new cp.Body(mass, cp.momentForBox(mass, width, height));
-
+			body.setPos(cp.v(posX, posY));
       this.space.addBody(body);
+		//	sprite.setBody(body);
+			sprite.setPosition(posX, posY);
+			this.addChild(sprite,5);
 
-      // 形状、摩擦係数、反発係数を設定
+			// 形状、摩擦係数、反発係数を設定
       var shape = new cp.BoxShape(body, width, height);
       shape.setFriction(friction);
       shape.setElasticity(elasticity);
-//トーテムであるフラグ
-      shape.setCollisionType(SpriteTag.totem);
-
+      shape.setCollisionType(type);
       this.space.addShape(shape);
+			shape.image = sprite;
+			sprite.shape = shape;
+			shapeArray.push(shape);
 
-      physicsSprite.setBody(body);
-      physicsSprite.setPosition(posX, posY);
-      this.addChild(physicsSprite);
-
-      return physicsSprite;
+			return sprite;
    },
 
    initSpace: function() {
@@ -102,7 +101,13 @@ blocks[3] = this.createDynamicObject(res.brick1x1_png, winWidth* 0.7,120+25*3,1,
       // 重力加速度
       this.space.gravity = cp.v(0, -980);
       // shapeを可視化する（デバッグ用）
-      this.addChild(new cc.PhysicsDebugNode(this.space));
+      this.addChild(new cc.PhysicsDebugNode(this.space, 999));
+      //物理エンジンで計算する地面領域を作る（これがないと計算されない）
+      var wallBottom = new cp.SegmentShape(this.space.staticBody,
+         cp.v(-4294967294, g_groundHeight), // start point
+         cp.v(4294967295, g_groundHeight), // MAX INT:4294967295
+         0); // thickness of wall
+      this.space.addStaticShape(wallBottom);
       this.scheduleUpdate();
    },
 
@@ -123,28 +128,28 @@ blocks[3] = this.createDynamicObject(res.brick1x1_png, winWidth* 0.7,120+25*3,1,
    },
 
    update: function(dt) {
-     // ランナーのスプライトとBodyの同期
+      // ランナーのスプライトとBodyの同期
       // 物理エンジンの更新
       this.space.step(dt);
-       for(i=0; i<blocks.length ;i++){
-         console.log( blocks[i].x)
-       }
-      //     var body =  blocks[i].getBody()
-      // // //      blocks[i].setPosition(body.p);
-      //       blocks[i].setPosition(-cc.radiansToDegrees(body.w));
-      //  }
-    },
+			 for (var i = 0; i < shapeArray.length; i++) {
+	         var shape = shapeArray[i];
+	         shape.image.x = shape.body.p.x;
+	         shape.image.y = shape.body.p.y;
+	         var angle = Math.atan2(-shape.body.rot.y, shape.body.rot.x);
+	         shape.image.rotation = angle * 180 / 3.14;
+	    }
+   },
 });
 
-var touchListener = cc.EventListener.create (
-  {
-    event: cc.EventListener.TOUCH_ONE_BY_ONE, // シングルタッチのみ対応
-    swallowTouches:false, // 以降のノードにタッチイベントを渡す
-   onTouchBegan:function(touch, event){ // タッチ開始時
-     var body = totem.getBody()
-     console.log(body)
-     //『body』に力を加えます
-     body.applyImpulse(cp.v(500, 500),cp.v(0,0))
+var touchListener = cc.EventListener.create({
+   event: cc.EventListener.TOUCH_ONE_BY_ONE, // シングルタッチのみ対応
+   swallowTouches: false, // 以降のノードにタッチイベントを渡す
+   onTouchBegan: function(touch, event) { // タッチ開始時
+      var body = totem.shape.body;
+      console.log(body)
+         //『body』に力を加えます
+      body.applyImpulse(cp.v(500, 0), cp.v(0, -20))
+         //body.applyForce(cp.v(500, 500),cp.v(0,0))
+
    }
-  }
-);
+});
